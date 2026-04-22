e8deb89324350c483d950ebc81cd7fa7a38c1d82
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-22 16:55:22 -0400

    feat: simple example switch

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int switchValue = 1;
 
+        switch(switchValue) {
+            case 1:
+                System.out.println("The value is 1");
+                break;
+            case 2:
+                System.out.println("The value is 2");
+                break;
+            default:
+                System.out.println("The value is not 1 or 2");
+        }
     }
 }
