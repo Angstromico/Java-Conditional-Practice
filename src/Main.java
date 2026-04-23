0e234257626a2a546705955676cdfc720659a041
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-23 07:23:00 -0400

    refactor: test multiple cases in switch

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int switchValue = 1;
+        int switchValue = 4;
 
         switch(switchValue) {
             case 1:
@@ -11,8 +11,11 @@ public class Main {
             case 2:
                 System.out.println("The value is 2");
                 break;
+            case 3: case 4: case 5:
+                System.out.println("The value is 3, 4 or 5");
+                break;
             default:
-                System.out.println("The value is not 1 or 2");
+                System.out.println("The value is not 1, 2, 3, 4 or 5");
         }
     }
 }
